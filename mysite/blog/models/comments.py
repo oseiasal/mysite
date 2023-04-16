72ae37c92b023c071fdbabbddbf8c09603d22e4e
Oseias Nascimento <oseiasal@gmail.com>
2023-04-16 12:37:56 -0300

    atualizações

---
+++ mysite/blog/models/comments.py
@@ -0,0 +1,16 @@
+from django.db import models
+from blog.models import Post
+
+class Comment (models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=88)
+    email = models. EmailField()
+    body = models.TextField()
+    created_on  = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ["created_on"]
+    
+    def str_(self):
+        return "Comment {} by {}".format(self.body, self.name)  
