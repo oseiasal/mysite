d2e9831fe1dc6b4f9f48a029cf423d146d37e6ed
Oseias Nascimento <oseiasal@gmail.com>
2023-04-26 14:02:27 -0300

    bug fix

--- mysite/blog/forms/forms.py
+++ mysite/blog/forms/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from blog.models import Comment
 from django import forms
 
 class CommentForm(forms.ModelForm):
--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from blog.models import Post
-from mysite.blog.forms import CommentForm
+from blog.forms import CommentForm
+
 
 class PostView(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -20,16 +21,16 @@ def post_detail(request, slug):
     new_comment = None
     # Comment posted
    
-    # if request.method == "POST":
-    #     comment_form = CommentForm(data=request.POST)
-    # if comment_form.is_valid():
-    #     # Create Comment object but don't save to database yet
-    #     new_comment = comment_form.save(commit=False)
-    #     #Assign the current post to the comment
-    #     new_comment.post = post # Save the comment to the database
-    #     new_comment.save()
-    # else:
-    #     comment_form = CommentForm()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        # Create Comment object but don't save to database yet
+        new_comment = comment_form.save(commit=False)
+        #Assign the current post to the comment
+        new_comment.post = post # Save the comment to the database
+        new_comment.save()
+    else:
+        comment_form = CommentForm()
     
     return render(
             request,
