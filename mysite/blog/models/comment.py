7b0dba5af765795427cb27fd134946fe81d343de
Oseias Nascimento <oseiasal@gmail.com>
2023-04-26 13:54:31 -0300

    bug corrigido

--- mysite/blog/models/comment.py
+++ mysite/blog/models/comment.py
@@ -2,7 +2,9 @@ from django.db import models
 
 from blog.models import Post
 
+
 class Comment (models.Model):
+    
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
