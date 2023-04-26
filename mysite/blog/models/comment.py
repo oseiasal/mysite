3ce78fcdd1d8bc0a0191610c7a2f7d6fa71741d5
Oseias Nascimento <oseiasal@gmail.com>
2023-04-26 11:09:42 -0300

    make migrations

--- mysite/blog/models/comment.py
+++ mysite/blog/models/comment.py
@@ -4,8 +4,8 @@ from blog.models import Post
 
 class Comment (models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    name = models.CharField(max_length=88)
-    email = models. EmailField()
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
     created_on  = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
@@ -13,5 +13,5 @@ class Comment (models.Model):
     class Meta:
         ordering = ["created_on"]
     
-    def str_(self):
+    def __str__(self):
         return "Comment {} by {}".format(self.body, self.name)  
