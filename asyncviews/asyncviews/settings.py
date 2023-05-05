9f1f72cb63b1f9e0c3051794c78d738dd74b8d6d
Oseias Nascimento <39315752+oseiasal@users.noreply.github.com>
2023-05-05 16:55:12 +0000

    base

--- asyncviews/asyncviews/settings.py
+++ asyncviews/asyncviews/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,3 +121,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+print(BASE_DIR)
